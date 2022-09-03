3c11a1732a2c1cd3056ec3502156ba437e68de94
Rishabh Upadhyay <na20b052@smail.iitm.ac.in>
2022-09-03 19:21:31 +0530

    [category].js

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,38 @@
-import { Card, Image, Text, Badge, Button, Group, Grid } from '@mantine/core';
+import { Card, Image, Text, Grid } from '@mantine/core';
 import Head from "next/head";
+import Link from 'next/link';
 
 
 
 const comptype = [
   {
+    href: "/hygiene",
     title: "Hygiene",
     imageurl: "https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
   },
   {
 
+    href: "",
     title: "Sports",
     imageurl: ""
   },
   {
+    href: "",
     title: "Staff",
     imageurl: ""
   },
   {
+    href: "",
     title: "Common Rooms",
     imageurl: ""
   },
   {
+    href: "",
     title: "Electrical",
     imageurl: ""
   },
   {
+    href: "",
     title: "Plumbing",
     imageurl: ""
   }
@@ -55,6 +62,7 @@ export default function Home() {
 
           return (
             <Grid.Col xs={8} sm={7} md={6} lg={4}>
+              <Link href={a.href}>
               <Card className="comptype" shadow="sm" p="lg" radius="md" withBorder>
                 <Card.Section>
                   <Image
@@ -64,6 +72,7 @@ export default function Home() {
                   <Text weight={500} style={{ padding: "0.5rem", width: "100%", textAlign: "right" }}>{a.title}</Text>
                 </Card.Section>
               </Card>
+              </Link>
             </Grid.Col>
           )
         })}
