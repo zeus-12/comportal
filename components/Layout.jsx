13a3ea208c56501450c1fe092592c342ad733395
Vishnu <vishnu@Vishnus-MacBook-Pro.local>
2022-09-03 11:30:32 +0530

    first commit

---
+++ components/Layout.jsx
@@ -0,0 +1,4 @@
+const Layout = ({ children }) => {
+  return <div>{children}</div>;
+};
+export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Layout from "../components/Layout";
+import { MantineProvider } from "@mantine/core";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        /** Put your mantine theme override here */
+        colorScheme: "dark",
+      }}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </MantineProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
